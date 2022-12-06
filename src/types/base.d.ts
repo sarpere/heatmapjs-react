b239d8dc348866ba661b7f7ccc648173d6be302a
Sarper Ekinci <sarperekinci@hotmail.com>
2022-12-06 18:32:17 +0300

    unit

--- src/types/base.d.ts
+++ src/types/base.d.ts
@@ -5,9 +5,10 @@ type percentValue = { x: number; y: number; value: number };
 type customValue = { [key: string]: number };
 type pxValue = number;
 type MapValue = percentValue | pxValue | customValue;
+type unit = keyof typeof units;
 interface HeatmapReactProps {
   max: number;
   data: Array<MapValue>;
-  unit?: units.decimal | units.percent;
+  unit?: unit;
   config?: config;
 }
